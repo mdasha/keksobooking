fd1362089880cf437b8d603618687831c867b699
mdasha <mdasha1979@yandex.ru>
2017-04-12 23:22:08 +0300

    Попыталась исправить код, чтобы все работало

--- js/map.js
+++ js/map.js
@@ -1,38 +1,41 @@
-'use strict';
+﻿'use strict';
 var offerDialog = document.querySelectorAll('#offer-dialog');
 var dialogClose = offerDialog[0].querySelector('.dialog__close');
 var dialogTitle = offerDialog[0].querySelectorAll('.dialog__title');
-var fragment = document.createDocumentFragment();
+var dialogPanel = offerDialog[0].querySelectorAll('.dialog__panel');
 // Все pin заводим в массив, присваиваем им атрибут data-index и отключаем для всех класс pin-active
 var deactivateClassPinActive = function () {
-  for (var i = 0; i < 9; i++) {
-    pinsArray.push(pins[i]);
-    pinsArray[i].setAttribute('data-index', i);
-    pinsArray[i].classList.remove('pin--active');
+  for (var k = 0; k < 9; k++) {
+    pinsArray.push(pins[k]);
+    pinsArray[k].setAttribute('data-index', k);
+    pinsArray[k].classList.remove('pin--active');
   }
 };
 // Замена карточки элемента
 var replaceOfferCard = function () {
-  var dialogPanel = offerDialog[0].querySelectorAll('.dialog__panel');
+  dialogPanel = offerDialog[0].querySelectorAll('.dialog__panel');
   if (dialogPanel[1]) {
     offerDialog[0].removeChild(dialogPanel[0]);
   }
 };
 // Удаление карточки элемента
 var deleteOfferCard = function () {
-  var dialogPanel = offerDialog[0].querySelectorAll('.dialog__panel');
+  dialogPanel = offerDialog[0].querySelectorAll('.dialog__panel');
   offerDialog[0].removeChild(dialogPanel[0]);
   dialogTitle[0].style.display = 'none';
   deactivateClassPinActive();
 };
+var newElement = {};
 var openUserDialog = function (data) {
+  var fragment = document.createDocumentFragment();
   dialogTitle[0].style.display = 'block';
 // Заменяем src у аватарки пользователя — изображения, которое записано в .dialog__title — на значения поля author.avatar отрисовываемого объекта
   offerDialog[0].querySelector('.dialog__title img').src = window.card.createCards[data].author.avatar;
 // Добавляем вновь созданный элемент dialog__panel
-  fragment.appendChild(renderOfferInDialogPanel(window.card.createCards[data].offer));
+  newElement = window.pin.createOffers[data];
+  fragment.appendChild(newElement);
   offerDialog[0].appendChild(fragment);
-// Удаляем для замены заполненный элемент dialog__panel
+    // Удаляем для замены заполненный элемент dialog__panel
   replaceOfferCard();
 };
 var pins = document.querySelectorAll('.pin');
@@ -54,12 +57,13 @@ var toolbarButtonHandler = function (e) {
     }
   });
 };
+var i = 0;
 // При нажатии на аватарку срабатывает функция
-for (var i = 0; i < pins.length; i++) {
-  pins[i].addEventListener('click', toolbarButtonHandler);
+for (var l = 0; l < 9; l++) {
+  pins[l].addEventListener('click', toolbarButtonHandler);
 }
 // Когда аватарка в фокусе при нажатии на enter открывается карточка объявления
-for (i = 0; i < pins.length; i++) {
+for (i = 0; i < 9; i++) {
   pins[i].addEventListener('keydown', function (evt) {
     if (evt.keyCode === 13) {
       toolbarButtonHandler(evt);
