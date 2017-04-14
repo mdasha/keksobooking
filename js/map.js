ee7bc0388b957b404abfd0de428a8ac4855c2eb5
mdasha <mdasha1979@yandex.ru>
2017-04-14 17:50:15 +0300

    Задание по перетаскиванию элемента

--- js/map.js
+++ js/map.js
@@ -11,7 +11,6 @@ var deactivateClassPinActive = function () {
     pinsArray[k].classList.remove('pin--active');
   }
 };
-
 // Удаление карточки элемента
 var deleteOfferCard = function () {
   dialogPanel = offerDialog.querySelectorAll('.dialog__panel');
@@ -69,3 +68,49 @@ dialogClose.addEventListener('keydown', function (evt) {
     deleteOfferCard();
   }
 });
+// Перетаскиваем элемент .main-pin
+var pinContainer = document.querySelector('.tokyo__pin-map');
+var draggedElement = pinContainer.querySelector('.pin__main');
+draggedElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+// Запомним координаты точки, с которой начали движение
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+// При каждом движении мыши нам нужно обновлять смещение относительно первоначальной точки, чтобы диалог смещался на необходимую величину
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    draggedElement.style.top = (draggedElement.offsetTop - shift.y) + 'px';
+    draggedElement.style.left = (draggedElement.offsetLeft - shift.x) + 'px';
+  // Заполняем поле адреса координатами в зависимости от перемещения .main_pin
+    var pinLengthHalf = 37.5;
+    var pinHeight = 94;
+    var NewPinX = parseInt(draggedElement.style.left, 10) + pinLengthHalf;
+    var NewPinY = parseInt(draggedElement.style.top, 10) + pinHeight;
+    var address = document.querySelector('#address');
+    address.value = 'x: ' + NewPinX + 'px, y: ' + NewPinY + 'рх';
+  };
+// При отпускании кнопки мыши нужно переставать слушать события движения мыши
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+// Добавляем обработчики события передвижения мыши и отпускания кнопки мыши
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
