a0598ecc49d0c8d8e891ebfd4adbf3716cb0d438
mdasha <mdasha1979@yandex.ru>
2017-04-20 23:52:03 +0300

    Подключение загружаемых с сервера данных

--- js/map.js
+++ js/map.js
@@ -4,62 +4,64 @@
   var dialogClose = offerDialog.querySelector('.dialog__close');
   var dialogTitle = offerDialog.querySelectorAll('.dialog__title');
   var dialogPanel = offerDialog.querySelectorAll('.dialog__panel');
+  window.load(function (onSuccess) {
+    var pins = document.querySelectorAll('.pin');
+    var pinsArray = [];
 // Все pin заводим в массив, присваиваем им атрибут data-index и отключаем для всех класс pin-active
-  var deactivateClassPinActive = function () {
-    for (var k = 0; k < 11; k++) {
-      pinsArray.push(pins[k]);
-      pinsArray[k].setAttribute('data-index', k);
-      pinsArray[k].classList.remove('pin--active');
-    }
-  };
+    var deactivateClassPinActive = function () {
+      for (var k = 0; k < 11; k++) {
+        pinsArray.push(pins[k]);
+        pinsArray[k].setAttribute('data-index', k);
+        pinsArray[k].classList.remove('pin--active');
+      }
+    };
 // Удаление карточки элемента
-  var deleteOfferCard = function () {
-    dialogPanel = offerDialog.querySelectorAll('.dialog__panel');
-    dialogPanel[0].style.display = 'none';
-    dialogTitle[0].style.display = 'none';
-    deactivateClassPinActive();
-  };
-  var pins = document.querySelectorAll('.pin');
-  var pinsArray = [];
-  var toolbarButtonHandler = function (e) {
+    var deleteOfferCard = function () {
+      dialogPanel = offerDialog.querySelectorAll('.dialog__panel');
+      dialogPanel[0].style.display = 'none';
+      dialogTitle[0].style.display = 'none';
+      deactivateClassPinActive();
+    };
+    var toolbarButtonHandler = function (e) {
 // Создаем массив элементов с классом pin и убираем со всех элементов класс .pin-active. При создании массива создаем специальный атрибут data-index
-    deactivateClassPinActive();
+      deactivateClassPinActive();
 // Добавляем класс .pin-active выделенному элементу
-    pins = e.currentTarget;
-    pins.classList.add('pin--active');
+      pins = e.currentTarget;
+      pins.classList.add('pin--active');
  // Получаем значение атрибута data-index текущего элемента и вызываем функцию, которая меняет данные об объекте и его фотку
-    var dataIndexPin = pins.getAttribute('data-index');
+      var dataIndexPin = pins.getAttribute('data-index');
 // Подключаем отдельным модулем (show-card.js) вызов функции, которая показывает карточку выбранного жилья по нажатию на метку на карте
-    window.showCard.openUserDialog(dataIndexPin);
+      window.showCard.openUserDialog(dataIndexPin);
  // При открытом диалоге клавиша esc закрывает его и деактивирует элемент с классом .pin
-    document.addEventListener('keydown', function (evt) {
-      if (evt.keyCode === 27) {
+      document.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === 27) {
+          deleteOfferCard();
+        }
+      });
+    };
+    var i = 0;
+// При нажатии на аватарку срабатывает функция
+    for (var l = 0; l < 11; l++) {
+      pins[l].addEventListener('click', toolbarButtonHandler);
+    }
+// Когда аватарка в фокусе при нажатии на enter открывается карточка объявления
+    for (i = 0; i < 11; i++) {
+      pins[i].addEventListener('keydown', function (evt) {
+        if (evt.keyCode === 13) {
+          toolbarButtonHandler(evt);
+        }
+      });
+    }
+// При нажатии на элемент .dialog-close скрываем карточку объявления
+    dialogClose.addEventListener('click', function () {
+      deleteOfferCard();
+    });
+// При открытом диалоге и фокусе на крестике нажатие клавиши ENTER приводит к закрытию диалога и деактивации активного элемента .pin
+    dialogClose.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
         deleteOfferCard();
       }
     });
-  };
-  var i = 0;
-// При нажатии на аватарку срабатывает функция
-  for (var l = 0; l < 11; l++) {
-    pins[l].addEventListener('click', toolbarButtonHandler);
-  }
-// Когда аватарка в фокусе при нажатии на enter открывается карточка объявления
-  for (i = 0; i < 11; i++) {
-    pins[i].addEventListener('keydown', function (evt) {
-      if (evt.keyCode === 13) {
-        toolbarButtonHandler(evt);
-      }
-    });
-  }
-// При нажатии на элемент .dialog-close скрываем карточку объявления
-  dialogClose.addEventListener('click', function () {
-    deleteOfferCard();
-  });
-// При открытом диалоге и фокусе на крестике нажатие клавиши ENTER приводит к закрытию диалога и деактивации активного элемента .pin
-  dialogClose.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 13) {
-      deleteOfferCard();
-    }
   });
 // Перетаскиваем элемент .main-pin
   var pinContainer = document.querySelector('.tokyo__pin-map');
