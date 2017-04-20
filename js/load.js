807705c6387fba27d8dd435adf11c6b73d2fe338
mdasha <mdasha1979@yandex.ru>
2017-04-20 23:51:43 +0300

    Подключение обработчика ошибок при загрузке данных с сервера

--- js/load.js
+++ js/load.js
@@ -10,11 +10,24 @@
       if (xhr.status === 200) {
         onSuccess(xhr.response);
       } else {
-        onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+        onError = (function () {
+          var header = document.querySelector('header');
+          var onErrorText = header.querySelector('.error');
+          if (!onErrorText) {
+            var fragment = document.createDocumentFragment();
+            var newElement = document.createElement('div');
+            newElement.className = 'error';
+            newElement.textContent = 'Произошла ошибка при загрузке данных. Код ошибки:' + xhr.status;
+            newElement.style.color = 'white';
+            newElement.style.backgroundColor = 'red';
+            fragment.appendChild(newElement);
+            header.appendChild(fragment);
+          }
+        })();
       }
     });
     xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
+      document.body.textContent = 'Произошла ошибка при загрузке данных';
     });
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
