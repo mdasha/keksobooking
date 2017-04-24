e33bdc01a94ef5d0ddcabdbf3296590f25bb8b05
mdasha <mdasha1979@yandex.ru>
2017-04-24 21:58:56 +0300

    Изменила кодировку

--- js/pin.js
+++ js/pin.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 window.pin = (function () {
   var similarOfferTemplate = document.querySelector('#lodge-template').content;
   // Заполнение блока DOM-элементами на основе JS-объектов
