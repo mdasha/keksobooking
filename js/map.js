e73475f08bac0f20e9f1f777662d0185d5b3f191
mdasha <mdasha1979@yandex.ru>
2017-04-27 07:44:40 +0300

    Попытка подключить фильтры

--- js/map.js
+++ js/map.js
@@ -11,11 +11,13 @@
     window.cards = [];
     window.cards = data;
 // Подключаем модуль отрисковки pin-ов на карте
-    var pins = window.renderOffers(data);
+    var pins = window.renderOffers(window.sameHousingTypes);
     var pinsArray = [];
+// Считаем количество отрисовываемых pin-ов с учетом главного pin-main
+    var pinLength = data.length + 1;
 // Все pin заводим в массив, присваиваем им атрибут data-index и отключаем для всех класс pin-active
     var deactivateClassPinActive = function () {
-      for (var k = 0; k < 11; k++) {
+      for (var k = 0; k < pinLength; k++) {
         pinsArray.push(pins[k]);
         pinsArray[k].setAttribute('data-index', k);
         pinsArray[k].classList.remove('pin--active');
@@ -47,11 +49,11 @@
     };
     var i = 0;
 // При нажатии на аватарку срабатывает функция
-    for (var l = 0; l < 11; l++) {
+    for (var l = 0; l < pinLength; l++) {
       pins[l].addEventListener('click', toolbarButtonHandler);
     }
 // Когда аватарка в фокусе при нажатии на enter открывается карточка объявления
-    for (i = 0; i < 11; i++) {
+    for (i = 0; i < pinLength; i++) {
       pins[i].addEventListener('keydown', function (evt) {
         if (evt.keyCode === 13) {
           toolbarButtonHandler(evt);
@@ -115,17 +117,141 @@
   });
 // Фильтруем элементы сначала по типу жилья
   var housingType = document.querySelector('#housing_type');
-  window.cards = [];
+  var housingPrice = document.querySelector('#housing_price');
+  var housingRoomNumber = document.querySelector('#housing_room-number');
+  var housingGuests = document.querySelector('#housing_guests-number');
+  var housingFeatures = Array.from(document.getElementsByName('feature'));
   var offerType;
+  var offerPrice = {};
+  var offerRoomNumber;
+  var offerGuests;
+  var offerFeatures = [];
   var updateOffers = function () {
-    var sameHousingTypes = window.cards.filter(function (it) {
-      return it.offer.type === offerType;
+    window.sameHousingTypes = window.cards;
+    if (housingType.value !== 'any') {
+      window.sameHousingTypes = window.cards.filter(function (it) {
+        return it.offer.type === offerType;
+      }); } else {
+      window.sameHousingTypes = window.cards;
+    }
+    var checkNumericRange = function (value) {
+      if (typeof value !== 'number') {
+        return false;
+      } else {
+        return value > offerPrice.min && value < offerPrice.max;
+      }
+    };
+    window.samePrice = window.cards.filter(function (it) {
+      return checkNumericRange(it.offer.price);
     });
-    window.renderOffers(sameHousingTypes);
+
+    window.filteredData = window.sameHousingTypes.concat(window.samePrice).concat(window.sameRoomNumbers);
+    window.filteredData2 = window.filteredData.filter(function (it, i) {
+      return window.filteredData.indexOf(it) !== i;
+    });
+
+    if (housingRoomNumber.value !== 'any') {
+      window.sameRoomNumbers = window.cards.filter(function (it) {
+        return it.offer.rooms === offerRoomNumber;
+      }); } else {
+      window.sameRoomNumbers = window.cards;
+    }
+    if (housingGuests.value !== 'any') {
+      window.sameGuests = window.cards.filter(function (it) {
+        return it.offer.guests === offerGuests;
+      }); } else {
+      window.sameGuests = window.cards;
+    }
+    window.sameFeatures = [];
+    for (i = 0; i < 6; i++) {
+      window.sameFeatures[i] = window.cards.filter(function (it) {
+        var k = 0;
+        if (it.offer.features.length > 0) {
+          for (var j = 0; j < it.offer.features.length; j++) {
+            if (it.offer.features[j] === offerFeatures[i]) {
+              k = j;
+            }
+          }
+        }
+        return it.offer.features[k] === offerFeatures[i];
+      });
+    }
+    console.log('По типу жилья ' + offerType);
+    console.log(window.sameHousingTypes);
+    console.log('По минимальной цене ' + offerPrice.min);
+    console.log(window.samePrice);
+    console.log('По числу комнат ' + offerRoomNumber);
+    console.log(window.sameRoomNumbers);
+    console.log('По числу гостей ' + offerGuests);
+    console.log(window.sameGuests);
+    console.log('По удобствам ' + offerFeatures);
+    console.log(window.sameFeatures);
+    console.log('Конкатенация');
+    console.log(window.filteredData);
+
+    window.renderOffers(window.sameHousingTypes);
   };
+
+// Фильтруем по типу жилья
   housingType.addEventListener('change', function (e) {
     housingType = e.currentTarget;
     offerType = housingType.value;
+    updateOffers();
+  });
+// Фильтруем по количеству комнат
+  housingRoomNumber.addEventListener('change', function (e) {
+    housingRoomNumber = e.currentTarget;
+    offerRoomNumber = parseInt(housingRoomNumber.value, 10);
+    updateOffers();
+  });
+// Фильтруем по количеству гостей
+  housingGuests.addEventListener('change', function (e) {
+    housingGuests = e.currentTarget;
+    offerGuests = parseInt(housingGuests.value, 10);
+    updateOffers();
+  });
+// Фильтруем по удобствам
+  var j = 0;
+  for (var i = 0; i < housingFeatures.length; i++) {
+    housingFeatures[i].addEventListener('click', function (e) {
+      housingFeatures[i] = e.currentTarget;
+      if (housingFeatures[i].checked) {
+        offerFeatures[housingFeatures.indexOf(housingFeatures[i])] = housingFeatures[i].value;
+      }
+      j = j + 1;
+      updateOffers();
+    });
+  }
+
+// Фильтруем по цене предложения
+  housingPrice.addEventListener('change', function (e) {
+    housingPrice = e.currentTarget;
+    switch (housingPrice.value) {
+      case 'low':
+        offerPrice = {
+          min: 0,
+          max: 9999
+        };
+        break;
+      case 'middle':
+        offerPrice = {
+          min: 10000,
+          max: 49999
+        };
+        break;
+      case 'high':
+        offerPrice = {
+          min: 50000,
+          max: Infinity
+        };
+        break;
+      default:
+        offerPrice = {
+          min: 0,
+          max: Infinity
+        };
+        break;
+    }
     updateOffers();
   });
 })();
